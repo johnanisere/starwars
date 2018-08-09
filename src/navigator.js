75fd90461c14cd62337a811695d2b0104cae9ac0
John <john@busha.co>
2018-08-09 17:40:38 +0100

    implement sort feature

--- src/navigator.js
+++ src/navigator.js
@@ -1,12 +1,29 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { createStackNavigator } from "react-navigation";
+
 import HomeScreen from "./containers/HomeScreen";
 import MoviesScreen from "./containers/MoviesScreen";
 
 export default createStackNavigator({
-  Movies: {
-    screen: MoviesScreen
-  },
   Home: {
-    screen: HomeScreen
+    screen: HomeScreen,
+    navigationOptions: {
+      header: null
+    }
+  },
+  Movies: {
+    screen: MoviesScreen,
+    navigationOptions: () => ({
+      title: `Movies`,
+      headerStyle: {
+        backgroundColor: "#000",
+        elevation: null,
+        borderBottomColor: `#f8e71c`
+      },
+      headerTitleStyle: {
+        color: `#f8e71c`
+      }
+    })
   }
 });
